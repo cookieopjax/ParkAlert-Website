bcd65575e6972e0cce8c822703106527668b87f6
keane90818 <keane9018@gmail.com>
2023-04-26 16:44:03 +0800

    add app-bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,28 +1,24 @@
 <template>
-  <v-card>
-    <v-layout>
-      <!-- <v-system-bar color="deep-purple darken-3"></v-system-bar> -->
+  <v-app>
+    <v-app-bar :elevation="10" color="secondary" prominent position="relative">
+      <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>My files1</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn variant="text" icon="mdi-magnify"></v-btn>
+      <v-btn variant="text" icon="mdi-filter"></v-btn>
+      <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
+    </v-app-bar>
 
-      <v-app-bar :elevation="10" color="red" prominent>
-        <v-app-bar-nav-icon variant="text" @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-navigation-drawer v-model="drawer" location="top" temporary>
+      <v-list :items="items"></v-list>
+    </v-navigation-drawer>
 
-        <v-toolbar-title>My files1</v-toolbar-title>
-
-        <v-spacer></v-spacer>
-
-        <v-btn variant="text" icon="mdi-magnify"></v-btn>
-
-        <v-btn variant="text" icon="mdi-filter"></v-btn>
-
-        <v-btn variant="text" icon="mdi-dots-vertical"></v-btn>
-      </v-app-bar>
-
-      <v-navigation-drawer v-model="drawer" location="top" temporary>
-        <v-list :items="items"></v-list>
-      </v-navigation-drawer>
-    </v-layout>
-  </v-card>
-  <slot />
+    <v-content>
+      <v-card>
+        <slot />
+      </v-card>
+    </v-content>
+  </v-app>
 </template>
 <script>
 export default {
@@ -56,3 +52,15 @@ export default {
   }
 };
 </script>
+<style lang="scss">
+.v-application__wrap {
+  backface-visibility: hidden;
+  display: flex;
+  flex-direction: column;
+  flex: 1 1 auto;
+  max-width: 100%;
+  min-height: 10vh;
+
+  position: relative;
+}
+</style>
