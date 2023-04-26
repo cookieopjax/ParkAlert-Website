48e0d529155023397816942493f6e09d6a35c881
cookie_opjax <vigor01695@gmail.com>
2023-04-26 11:02:11 +0800

    Setup eslint and prettier

--- pages/index.vue
+++ pages/index.vue
@@ -1,23 +1,22 @@
 <template>
+  <div>
     <div>官網</div>
     <v-container id="topicImg">
-    <v-row no-gutters>
-      <v-col
-      >
-        <v-sheet class="ma-2 pa-2">
-          One of three columns
-        </v-sheet>
-      </v-col>
-    </v-row>
-  </v-container>
+      <v-row no-gutters>
+        <v-col>
+          <v-sheet class="ma-2 pa-2"> One of three columns </v-sheet>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 <style lang="scss">
-    $main-color: #F7F5E6;
-    body{
-        background-color: $main-color;
-    }
-    #topicImg{
-        background-color: white;
-        height: 30rem;
-    }   
+$main-color: #f7f5e6;
+body {
+  background-color: $main-color;
+}
+#topicImg {
+  background-color: white;
+  height: 30rem;
+}
 </style>
