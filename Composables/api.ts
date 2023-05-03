6a765ce3f43a7dbf41e7522b5c9eededd129ea93
keane90818 <keane9018@gmail.com>
2023-05-03 11:05:40 +0800

    done jwt

--- Composables/api.ts
+++ Composables/api.ts
@@ -1,42 +1,13 @@
 import axios from "axios";
-
-
-// export default {
-//   axios: {
-//     baseURL: 'https://parkalert.onrender.com',
-//     credentials: true,
-//     withCredentials:false,
-//     interceptors: {
-//       request: (config :any) => {
-//         console.log("interceptors request");
-//         const token = localStorage.getItem("token");
-//           if (token) {
-//             config.headers.Authorization = `Bearer ${token}`; // 如果 token 存在，就在 Authorization Header 中攜帶
-//             console.log("Authorization");
-//           }
-//         config.withCredentials = true;
-//         return config;
-//       },
-//       response: (response :any) => {
-//         console.log('interceptors response')
-//         return response
-//       }
-//     }
-//   }
-// }
-
 const instance = axios.create({
   // to-do : use .env file to control this
   baseURL: "https://parkalert.onrender.com"
 });
 
 instance.interceptors.request.use((config) => {
-  // console.log("interceptors request");
-      const token = localStorage.getItem("token");
-      // console.log(token);
+      const token = localStorage.getItem("token");// 從localStorage抓取token
       if (token) {
         config.headers.Authorization = "Bearer " +token; // 如果 token 存在，就在 Authorization Header 中攜帶
-        // console.log("Authorization");
       }
   config.withCredentials = false;
   return config;
--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,19 +1,6 @@
-import axios from "axios";
 import { apiIsAuth } from "../composables/api";
 
-const instance = axios.create({
-  // to-do : use .env file to control this
-  baseURL: "https://parkalert.onrender.com"
-});
 
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token');
-  if (token) {
-    config.headers["Authorization"] = `Bearer ${token}`;// 如果 token 存在，就在 Authorization Header 中攜帶
-    console.log("Authorization");
-  }
-  return config;
-});
 
 async function isAuth() {
   try {
@@ -27,6 +14,8 @@ async function isAuth() {
     return false;
   }
 }
+
+
 
 
 export default defineNuxtRouteMiddleware(async (to, from) => {
