81687637a467cece5f431558f92caca7b076f195
cookie_opjax <vigor01695@gmail.com>
2023-05-03 11:41:39 +0800

    Project structure adjustment

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,28 +1,10 @@
-import axios from "axios";
 import { apiIsAuth } from "../composables/api";
-
-const instance = axios.create({
-  // to-do : use .env file to control this
-  baseURL: "https://parkalert.onrender.com"
-});
-
-instance.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`; // 如果 token 存在，就在 Authorization Header 中攜帶
-  }
-  return config;
-});
 
 async function isAuth() {
   try {
     await apiIsAuth();
     return true;
   } catch (e: any) {
-    if (e.response.status === 401) {
-      // 提示使用者帳密錯誤...
-      console.log("401 error");
-    }
     return false;
   }
 }
